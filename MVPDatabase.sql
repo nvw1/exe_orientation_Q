9d7e50bbac17e335e68fd53ef01426b659977d66
GalacticRhino <48519465+GalacticRhino@users.noreply.github.com>
2020-02-24 12:14:40 +0000

    Create MVPDatabase.sql
    MVP database with single treasure trail implemented

---
+++ MVPDatabase.sql
@@ -0,0 +1,30 @@
+CREATE TABLE IF NOT EXISTS mvp_table(
+    hunt_id INT PRIMARY KEY,
+    node_number INT NOT NULL,
+    question_text VARCHAR(255),
+    answer VARCHAR (25)
+)
+
+INSERT INTO mvp_table
+VALUES(
+    1, 1, 'How many computers are there in the LoveLac room?','37' 
+
+);
+
+INSERT INTO mvp_table
+VALUES(
+    1, 2, 'Which lab in the innovation centre has Windows PC?','Babbage' 
+
+);
+
+INSERT INTO mvp_table
+VALUES(
+    1, 3, 'How much are the rice dishes for students at the innovation cafe?','4.50' 
+
+);
+
+INSERT INTO mvp_table
+VALUES(
+    1, 4, 'How many floors does the Harrison Building have?','4' 
+
+);
