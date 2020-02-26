4ea5924a49804316152fd0b7afec682452f904e2
Sam Rapier <samrapier@live.co.uk>
2020-02-26 21:35:18 +0000

    renamed deploy-django-to-azure

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deploy-django-to-azure.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deploy_django_to_azure.settings.production")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
