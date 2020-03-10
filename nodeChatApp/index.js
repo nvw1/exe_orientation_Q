dbb9289add3480b85ed1b4fe1d23ba37eb7f4bfd
arch563 <archiepullan+git@gmail.com>
2020-03-10 14:50:26 +0000

    lots of node things, have fun

---
+++ nodeChatApp/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+var namespace = io.of('/chatRooms');
+app.get('/', function(req, res) {
+    res.render('indexold.ejs')
+});
+
+users = {};
+
+/*
+io.sockets.on('connection', function(socket) {
+    console.log("user connected");
+
+socket.on('new', function(data, callback) {
+    console.log(data.name);
+    if(data in users)
+    callback(false);
+    else {
+        callback(true);
+        socket.name = data.name;
+        users[socket.name] = socket;
+    }
+})
+socket.on('msg', function(data,callback) {
+    callback(data.msg);
+    io.to(users[data.to].emit('priv',data.msg));
+})
+})
+*/
+
+var glob_room;
+namespace.on('connection', function(socket) {
+    socket.on('room', function(room) {
+        glob_room = room;
+        socket.join(glob_room);
+        socket.to(glob_room).emit("you're in this room" + room);
+    }) 
+    socket.on('username', function(username) {
+        socket.username = username;
+    });
+
+
+    socket.on('chat_message', function(message) {
+        namespace.to(glob_room).emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    });
+
+});
+
+
+const server = http.listen(8080, function() {
+    console.log('listening on *:8080');
+});
