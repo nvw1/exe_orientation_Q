6597e3e88f503e9b779d8c072dbac9fca2f58ba3
nvw1 <n.v.wenzler@gmail.com>
2020-03-12 06:25:28 +0000

    deleting redudant files and refactor

--- app/test.py
+++ app/test.py
@@ -1,3 +1,5 @@
+# author : Nik, Hao
+
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from app.views import *
@@ -5,6 +7,9 @@ from app.views import *
 class TestUrls(SimpleTestCase):
 
     def test_(self):
+        """
+        
+        """
         url = reverse('redirect')
         self.assertEqual(resolve(url).func, redirect)
 
